407c4ac4c38305d02ced8a55e237d4a2f1b0e0e8
muYuZhe <1211194480@qq.com>
2020-12-15 15:16:03 +0800

    分离出、入口文件

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import _ from 'Lodash';
 import './mySass/css/mySass.css'
+import printMe from './print.js'
 function component() {
     // 在document对象中创建一个元素div
     var element = document.createElement('div');
+    //新增一个按钮
+    var btn = document.createElement('button')
 
+    // 配置按钮内显示的内容和点击事件
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    // 按钮作为div的子节点
+    element.appendChild(btn);
     element.classList.add('test')
     // 对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ const path = require('path');
 
 module.exports = {
   // 设置入口路径
-  entry: './src/index.js',
+  entry:{
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   // 自定义输出
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module:{
