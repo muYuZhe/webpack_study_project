6327e391cd6096d718233972c30b173553b21495
muYuZhe <1211194480@qq.com>
2020-12-15 14:50:25 +0800

    css资源管理

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from 'Lodash';
+import './mySass/css/mySass.css'
 function component() {
     // 在document对象中创建一个元素div
     var element = document.createElement('div');
-  
+
+    element.classList.add('test')
     // 对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
