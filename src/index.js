2434158ef7f175419f8bc7a32ffdcfd717d198cd
muYuZhe <1211194480@qq.com>
2020-12-18 15:35:00 +0800

    生产环境构建

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 // import _ from 'Lodash';
 import './mySass/css/mySass.css'
-// import printMe from './print.js'
+import printMe from './print.js'
 import { cube } from './math.js';
+
+if (process.env.NODE_ENV!=='production') {
+  console.log('Looks like we are in development mode!');
+}
+
 function component() {
     // 在document对象中创建一个元素div
     var element = document.createElement('pre');
     //新增一个按钮
-    // var btn = document.createElement('button')
-
+    var btn = document.createElement('button')
     element.classList.add('test')
     // 对于执行这一行是必需的
     element.innerHTML = [
            'Hello webpack!',
            '5 cubed is equal to ' + cube(5)
          ].join('\n\n');
-     // 配置按钮内显示的内容和点击事件
-    //  btn.innerHTML = 'Click me and check the console!';
-    //  btn.onclick = printMe;
+    // 配置按钮内显示的内容和点击事件
+     btn.innerHTML = 'Click me and check the console!';
+     btn.onclick = printMe;
  
-     // 按钮作为div的子节点
-    //  element.appendChild(btn);
+    // 按钮作为div的子节点
+     element.appendChild(btn);
   
     return element;
   }
--- src/print.js
+++ src/print.js
@@ -1,3 +1,3 @@
 export default function printMe() {
-    console.log('I get called from print.js,I change333');
+    console.log('I get called from print.js,I change444');
 }
